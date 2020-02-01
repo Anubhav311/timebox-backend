e92bfd4397171700d5992c6af0fa244d9dfe2d10
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-01 23:55:26 +0530

    ran migrations

---
+++ data/migrations/20200201213550_001.js
@@ -0,0 +1,67 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+
+        tbl.datetime('created_at')
+            .defaultTo(knex.fn.now());
+
+        tbl.string('name');
+
+        tbl.string('email')
+            .notNullable();
+
+        tbl.string('password')
+            .notNullable();
+
+        tbl.integer('timebox')
+            .defaultTo(10)
+            .notNullable();
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+
+        tbl.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+
+        tbl.datetime('created_at')
+            .defaultTo(knex.fn.now());
+
+        tbl.datetime('due_at')
+            .defaultTo(knex.fn.now());
+
+        tbl.string('task')
+            .notNullable();
+
+        tbl.integer('completion_status')
+            .defaultTo(0)
+            .notNullable();
+    })
+    .createTable('subtasks', tbl => {
+        tbl.increments();
+
+        tbl.integer('task_id')
+            .notNullable()
+            .references('id')
+            .inTable('tasks')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+
+        tbl.string('subtask')
+            .notNullable();
+
+        tbl.integer('completion_status')
+            .defaultTo(0)
+            .notNullable();
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('subtasks')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('users')
+};
