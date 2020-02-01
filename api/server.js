bbdbafba04aef743c89da7371c771163b4d22c42
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-01 21:24:28 +0530

    added knexfile

--- api/server.js
+++ api/server.js
@@ -4,17 +4,17 @@ const cors = require('cors');
 
 const server = express();
 
-const usersRouter = require('../router/users');
-const skillsRouter = require('../router/skills');
-const partsRouter = require('../router/parts');
+// const usersRouter = require('../router/users');
+// const skillsRouter = require('../router/skills');
+// const partsRouter = require('../router/parts');
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json())
 
-server.use('/api/users', usersRouter);
-server.use('/api/skills', skillsRouter);
-server.use('/api/parts', partsRouter);
+// server.use('/api/users', usersRouter);
+// server.use('/api/skills', skillsRouter);
+// server.use('/api/parts', partsRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json("it's working")
---
+++ knexfile.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+
+};
