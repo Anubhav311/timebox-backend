2b0bcd98bafdb45bb616571ff8821c5cb930d070
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-12 21:10:22 +0530

    created subtasks router

---
+++ router/subtasksRouter.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const subtasksDb = require('../model/subtasksModel');
+
+
+router.get('/', (req, res) => {
+    filter = req.query
+    subtasksDb.getMany(filter)
+    .then(skills => {
+        res.status(200).json(skills)
+    })
+    .catch(err => res.send(err));
+});
+
+router.post('/', (req, res) => {
+    payload = req.body
+    subtasksDb.insert(payload)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => res.send(err.message));
+})
+
+router.put('/', (req, res) => {
+    subtasksDb.update({id: req.body.id}, req.body.payload)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => res.send(err));
+})
+
+router.delete('/', (req, res) => {
+    subtasksDb.remove(req.body)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => res.send(err));
+})
+
+module.exports = router;
