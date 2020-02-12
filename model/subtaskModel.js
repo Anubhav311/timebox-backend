a4754464ae1f44fd8ae574b319570aadce7a3955
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-12 21:08:55 +0530

    created model for subtasks

---
+++ model/subtaskModel.js
@@ -0,0 +1,45 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+    insert,
+    getMany,
+    update,
+    remove,
+}
+
+function insert(user) {
+    return db('subtasks')
+        .insert(user)
+        .returning('id')
+        .then(idArr => {
+            const id = idArr[0]
+            return db('subtasks')
+                .where({id}).first();
+        })
+}
+
+function getMany(filter = {}) {
+    return db('subtasks')
+        .where(fiilter)
+}
+
+function update(filter = {}, payload) {
+    return db('subtasks')
+        .where(filter)
+        .update(payload)
+        .returning('id')
+        .then(idArr => {
+            const id = idArr[0]
+            return db('subtasks')
+                .where({id})
+                .first();
+        })
+}
+
+function remove(filter = null) {
+    if (!filter) return new Error('No selector provided for deletion');
+    return db('subtasks')
+        .where(filter)
+        .delete()
+        .returning('id');
+}
